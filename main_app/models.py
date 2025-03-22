f433a7d2f6878690d76e603991469363e632dd74
mhurst <matthewhurst1791@gmail.com>
2025-03-22 17:42:28 -0400

    database created admin site setup issue with if statement in index

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+GEARTYPE = (
+    ('O', 'Other'),
+    ('G', 'Guitar'),
+    ('B', 'Bass'),
+    ('P', 'Percussion'),
+)
+
+class Gear(models.Model):
+
+    type = models.CharField(
+        max_length=1,
+        choices=GEARTYPE,
+        default=GEARTYPE[0][0]
+    )
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.model
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-
-class Gear:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-gears = [
-    Gear('Lolo', 'tabby', 'Kinda rude.', 3),
-    Gear('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Gear('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Gear('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
+from .models import Gear
 
 def home(request):
     return render(request, 'home.html')
@@ -23,4 +8,5 @@ def about(request):
     return render(request, 'about.html')
 
 def gear_index(request):
+    gears = Gear.objects.all()
     return render(request, 'gears/index.html', {'gears': gears})
