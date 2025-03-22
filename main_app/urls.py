42ea1b4bb3303e29290f87e54f683b52fe42a3a8
mhurst <matthewhurst1791@gmail.com>
2025-03-22 15:12:27 -0400

    home page template created static images working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('gear/', views.gear_index, name='gear-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Gear:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+gears = [
+    Gear('Lolo', 'tabby', 'Kinda rude.', 3),
+    Gear('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Gear('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Gear('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 
 def home(request):
+    return render(request, 'home.html')
 
-    return HttpResponse('<h1>Gear Page</h1>')
+def about(request):
+    return render(request, 'about.html')
+
+def gear_index(request):
+    return render(request, 'gears/index.html', {'gears': gears})
